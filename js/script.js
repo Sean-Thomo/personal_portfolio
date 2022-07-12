76fbed12950d5d542245377663fd1be66759c2eb
siphesihle-thomo <seansthomo@gmail.com>
2022-07-12 12:36:11 +0200

    Style navbar dark

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ const navSlide = () => {
 			if (link.style.animation) {
 				link.style.animation = ``;
 			} else {
-				link.style.animation = `navLinkFade 0.5s ease forwards ${
+				link.style.animation = `navLinkFade 0.3s ease forwards ${
 					index / 7 + 0.5
 				}s`;
 			}
